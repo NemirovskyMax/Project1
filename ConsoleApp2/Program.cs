a32501b90a7ba6e4e8eb1585f6b1cdc47d3e5151
NemirovskyMax <artemjaik@gmail.com>
2024-12-10 10:40:48 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static string GetMonthName(int month)
+        {
+            switch (month)
+            {
+                case 1: return "Январь";
+                case 2: return "Февраль";
+                case 3: return "Март";
+                case 4: return "Апрель";
+                case 5: return "Май";
+                case 6: return "Июнь";
+                case 7: return "Июль";
+                case 8: return "Август";
+                case 9: return "Сентабрь";
+                case 10: return "Октябрь";
+                case 11: return "Ноябрь";
+                case 12: return "Декабрь";
+                default: return "Ошибка";
+
+            }
+        }
+            static int Input(string message)
+            {
+                Console.WriteLine(message);
+                return int.Parse(Console.ReadLine());
+            }
+            static void Main(string[] args)
+        {
+            int month = Input("Номер месяца");
+            Console.WriteLine(GetMonthName(month));
+            Console.ReadKey();
+        }
+    }
+}
