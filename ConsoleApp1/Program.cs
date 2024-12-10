be14352fd8aff498c6872c22b72f3c6ae61e9bac
NemirovskyMax <artemjaik@gmail.com>
2024-12-10 10:23:05 +0300

    Project1
    10.12.24

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static string Days(int day)
+        {
+            switch (day)
+            {
+                case 1: return "Понедельник";
+                case 2: return "Вторник";
+                case 3: return "Среда";
+                case 4: return "Четверг";
+                case 5: return "Пятница";
+                case 6: return "Суббота";
+                case 7: return "Воскресенье";
+                default: return "Ошибка";
+            }
+        }
+        static int Input(string message)
+        {
+            Console.WriteLine(message);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Main(string[] args)
+        {
+            int day = Input("Номер дня");
+            Console.WriteLine(Days(day));
+            Console.ReadKey();
+        }
+    }
+}
